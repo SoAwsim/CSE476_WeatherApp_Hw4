63857376947690aa2b70b95999e1848af7230eee
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-12-08 01:41:18 +0300

    feat: add weather mini widget support

--- app/src/main/java/com/example/cse476/weatherapphw4/widget/CustomWeatherWidget.kt
+++ app/src/main/java/com/example/cse476/weatherapphw4/widget/CustomWeatherWidget.kt
@@ -2,21 +2,24 @@ package com.example.cse476.weatherapphw4.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.LayoutInflater
-import androidx.cardview.widget.CardView
+import com.example.cse476.weatherapphw4.R
 import com.example.cse476.weatherapphw4.databinding.WeatherCardWidgetBinding
+import com.google.android.material.card.MaterialCardView
 import java.util.Locale
 
-class CustomWeatherWidget @JvmOverloads constructor(
+class CustomWeatherWidget(
     context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : CardView(context, attrs, defStyleAttr) {
-    private val binding: WeatherCardWidgetBinding =
-        WeatherCardWidgetBinding.inflate(LayoutInflater.from(context), this, true)
+    attrs: AttributeSet
+) : MaterialCardView(context, attrs) {
+    private val binding: WeatherCardWidgetBinding
+
+    init {
+        inflate(context, R.layout.weather_card_widget, this)
+        this.binding = WeatherCardWidgetBinding.bind(this)
+    }
 
     fun setWeatherText(weather: String) {
-        this.binding.weatherStatusTextView.text = weather
+        this.binding.weatherDescriptionTextView.text = weather
     }
 
     fun setDayText(day: String) {
