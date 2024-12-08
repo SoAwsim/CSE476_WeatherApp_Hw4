e282cdfdae323ede97d12dc0d34bed4e3fffbbe7
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-12-08 04:32:50 +0300

    feat: add location support

--- app/src/main/java/com/example/cse476/weatherapphw4/WeatherActivity.kt
+++ app/src/main/java/com/example/cse476/weatherapphw4/WeatherActivity.kt
@@ -1,9 +1,14 @@
 package com.example.cse476.weatherapphw4
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.cse476.weatherapphw4.databinding.ActivityMainBinding
@@ -12,6 +17,10 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class WeatherActivity : AppCompatActivity() {
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 100
+    }
+
     private val model: WeatherViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
 
@@ -24,6 +33,35 @@ class WeatherActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        locationProvided()
+    }
+
+    private fun locationProvided() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            this.model.startLocationService()
+            return
+        }
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            LOCATION_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_REQUEST_CODE && grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            this.locationProvided()
+        } else {
+            Toast.makeText(this, "Location not allowed", Toast.LENGTH_SHORT).show()
         }
     }
 }
