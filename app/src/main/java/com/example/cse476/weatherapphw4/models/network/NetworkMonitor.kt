d43b18bea3b1a8b2bfd7c7931142fa8c5d4a8db6
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-12-08 03:07:00 +0300

    feat: add support for network state monitoring

---
+++ app/src/main/java/com/example/cse476/weatherapphw4/models/network/NetworkMonitor.kt
@@ -0,0 +1,40 @@
+package com.example.cse476.weatherapphw4.models.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class NetworkMonitor(context: Context) {
+    private val _connectivityManager =
+        context.getSystemService(ConnectivityManager::class.java) as ConnectivityManager
+
+    private val _networkState = MutableLiveData<Boolean>()
+    val networkState: LiveData<Boolean> = this._networkState
+
+    private val _networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            this@NetworkMonitor._networkState.postValue(true)
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            this@NetworkMonitor._networkState.postValue(false)
+        }
+    }
+
+    fun startMonitoringNetwork() {
+        val request = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+        this._connectivityManager.registerNetworkCallback(request, this._networkCallback)
+    }
+
+    fun stopMonitoringNetwork() {
+        this._connectivityManager.unregisterNetworkCallback(this._networkCallback)
+    }
+}
