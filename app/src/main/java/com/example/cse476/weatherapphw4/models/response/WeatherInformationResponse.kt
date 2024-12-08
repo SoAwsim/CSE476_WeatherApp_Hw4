c5aa246e782eeaf568f19a4b7284cef06a28e881
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-12-08 05:33:10 +0300

    fix: change response object to match the free api

--- app/src/main/java/com/example/cse476/weatherapphw4/models/response/WeatherInformationResponse.kt
+++ app/src/main/java/com/example/cse476/weatherapphw4/models/response/WeatherInformationResponse.kt
@@ -2,29 +2,40 @@ package com.example.cse476.weatherapphw4.models.response
 
 data class WeatherResponse(
     val dt: Long,
-    val sunrise: Long,
-    val sunset: Long,
-    val temp: Temp,
-    val feels_like: FeelsLike,
+    val main: Main,
+    val weather: List<Weather>,
+    val clouds: Clouds,
+    val wind: Wind,
+    val visibility: Int,
+    val pop: Int,
+    val sys: Sys,
+    val dt_txt: String
+)
+
+data class Main(
+    val temp: Double,
+    val feels_like: Double,
+    val temp_min: Double,
+    val temp_max: Double,
     val pressure: Int,
+    val sea_level: Int,
+    val grnd_level: Int,
     val humidity: Int,
-    val weather: List<Weather>
+    val temp_kf: Double
 )
 
-data class Temp(
-    val day: Double,
-    val min: Double,
-    val max: Double,
-    val night: Double,
-    val eve: Double,
-    val morn: Double
+data class Clouds (
+    val all: Int
 )
 
-data class FeelsLike(
-    val day: Double,
-    val night: Double,
-    val eve: Double,
-    val morn: Double
+data class Wind (
+    val speed: Double,
+    val deg: Int,
+    val gust: Double,
+)
+
+data class Sys (
+    val pod: String
 )
 
 data class Weather(
