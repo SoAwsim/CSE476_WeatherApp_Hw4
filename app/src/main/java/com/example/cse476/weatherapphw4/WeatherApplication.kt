fc9b5875d70f65d597259cff193da23fead0969c
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-12-08 03:19:37 +0300

    feat: add dependency injection

---
+++ app/src/main/java/com/example/cse476/weatherapphw4/WeatherApplication.kt
@@ -0,0 +1,7 @@
+package com.example.cse476.weatherapphw4
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApplication : Application() { }
