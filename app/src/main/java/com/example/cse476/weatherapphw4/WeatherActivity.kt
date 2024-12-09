d4e4a079c235b33c059371afa969726cb875c862
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-12-10 00:29:00 +0300

    feat: observer view model and fix broken binding

--- app/src/main/java/com/example/cse476/weatherapphw4/WeatherActivity.kt
+++ app/src/main/java/com/example/cse476/weatherapphw4/WeatherActivity.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.cse476.weatherapphw4.databinding.ActivityMainBinding
+import com.example.cse476.weatherapphw4.extensions.capitalizeEveryWord
+import com.example.cse476.weatherapphw4.extensions.toUIString
 import com.example.cse476.weatherapphw4.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,11 +21,19 @@ class WeatherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         this.binding = ActivityMainBinding.inflate(this.layoutInflater)
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(this.binding.main)
+        ViewCompat.setOnApplyWindowInsetsListener(this.binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        this.model.todayTemp.observe(this) { temp ->
+            this.binding.todayWeatherTempTextView.text = temp.toUIString()
+        }
+
+        this.model.todayWeatherStatus.observe(this) { status ->
+            this.binding.todayWeatherDescriptionTextView.text = status?.capitalizeEveryWord() ?: "NaN"
         }
     }
 }
