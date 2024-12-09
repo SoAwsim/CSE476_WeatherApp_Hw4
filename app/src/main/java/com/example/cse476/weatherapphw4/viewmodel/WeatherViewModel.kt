1ca49dd6217d436bc1e8b605f2b91f18b657faf4
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-12-10 00:28:44 +0300

    feat: add weather service usage to the main view model

--- app/src/main/java/com/example/cse476/weatherapphw4/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/example/cse476/weatherapphw4/viewmodel/WeatherViewModel.kt
@@ -2,6 +2,7 @@ package com.example.cse476.weatherapphw4.viewmodel
 
 import android.app.Application
 import android.graphics.Bitmap
+import android.icu.util.Calendar
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -13,8 +14,8 @@ import javax.inject.Inject
 @HiltViewModel
 class WeatherViewModel @Inject constructor(
     application: Application,
-    weatherService: WeatherService,
-    locationService: LocationService
+    private val weatherService: WeatherService,
+    private val locationService: LocationService
 ) : AndroidViewModel(application) {
     companion object {
         const val TAG = "WeatherViewModel"
@@ -23,9 +24,41 @@ class WeatherViewModel @Inject constructor(
     private val _todayTemp = MutableLiveData<Double?>(null)
     val todayTemp : LiveData<Double?> = this._todayTemp
 
-    private val _todayWeatherStatus = MutableLiveData<String?>(null)
+    private val _todayWeatherStatus = MutableLiveData<String?>()
     val todayWeatherStatus: LiveData<String?> = this._todayWeatherStatus
 
     private val _todayWeatherBitmapImage = MutableLiveData<Bitmap?>(null)
     val todayWeatherBitmapImage: LiveData<Bitmap?> = this._todayWeatherBitmapImage
+
+    init {
+        this.transformWeatherData()
+    }
+
+    fun transformWeatherData() {
+        val weatherData = this.weatherService.weatherMapByDate
+        val calendar = Calendar.getInstance()
+        val today = calendar.get(Calendar.DAY_OF_WEEK)
+        val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        val tomorrow = calendar.get(Calendar.DAY_OF_WEEK)
+        var usedTomorrow = false
+
+        var currentWeather = weatherData[today]
+        if (currentWeather == null) {
+            usedTomorrow = true
+            currentWeather = weatherData[tomorrow] ?: throw Exception("Illegal state")
+        }
+        val currentTimeWeather = if (usedTomorrow) {
+            currentWeather.first()
+        } else {
+            currentWeather.firstOrNull {
+                val listCalendar = Calendar.getInstance()
+                listCalendar.timeInMillis = it.dt * 1000L
+                currentHour >= listCalendar.get(Calendar.HOUR_OF_DAY)
+            } ?: currentWeather.first()
+        }
+
+        this._todayTemp.value = currentTimeWeather.main.temp
+        this._todayWeatherStatus.value = currentTimeWeather.weather.firstOrNull()?.description
+    }
 }
