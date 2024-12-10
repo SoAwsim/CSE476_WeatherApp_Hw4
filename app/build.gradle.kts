f31b90c0231ace9b8ae8b2812f59143c5f234482
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-12-10 23:27:25 +0300

    chore: add dependency for bottom navigation with fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 dependencies {
     implementation(libs.gson.v2110)
     implementation(libs.hilt.android)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     kapt(libs.hilt.android.compiler)
     implementation(libs.gson)
     implementation(libs.androidx.core.ktx)
