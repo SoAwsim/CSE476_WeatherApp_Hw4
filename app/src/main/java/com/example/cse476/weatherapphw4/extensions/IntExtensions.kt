feff0aa77be13904234d098346cd06dd0e74c6e4
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-12-11 06:33:41 +0300

    refactor: return full names of days

--- app/src/main/java/com/example/cse476/weatherapphw4/extensions/IntExtensions.kt
+++ app/src/main/java/com/example/cse476/weatherapphw4/extensions/IntExtensions.kt
@@ -2,12 +2,12 @@ package com.example.cse476.weatherapphw4.extensions
 
 fun Int.convertFromCalendarEnum(): String =
     when(this) {
-        1 -> "Sun"
-        2 -> "Mon"
-        3 -> "Tue"
-        4 -> "Wed"
-        5 -> "Thu"
-        6 -> "Fri"
-        7 -> "Sat"
+        1 -> "Sunday"
+        2 -> "Monday"
+        3 -> "Tuesday"
+        4 -> "Wednesday"
+        5 -> "Thursday"
+        6 -> "Friday"
+        7 -> "Saturday"
         else -> "Unknown"
     }
